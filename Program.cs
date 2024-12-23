672d4418f1894b5993ac7fbaefe4d65e97553b31
Maksim Smirnov <115401277+Smargen2305@users.noreply.github.com>
2024-12-23 19:39:39 +0500

    new pull request

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
 
-            bool continueProgram = true;
+            bool continueProgram = true; /// pull request
+
 
             while (continueProgram)
             {
